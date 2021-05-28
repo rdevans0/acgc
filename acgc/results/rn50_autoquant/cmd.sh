783477d1b3e8edb6fae18c1267adc3c71a728968
Dave Evans <rdevans0@gmail.com>
2021-05-27 17:58:10 -0700

    Anonymize cmd.sh and remove extra folders

--- acgc/results/rn50_autoquant/cmd.sh
+++ acgc/results/rn50_autoquant/cmd.sh
@@ -1,3 +1,1 @@
-pushd /home/rdevans/perforce/home/rdevans/papers/2021_neurips_autoquant/code/acgc
 python3 train_cifar_act_error.py --model resnet50 -d 0 --dataset cifar10 --out rn50_autoquant --grad_approx mean 100 10 --ae_autoquant 0 CNV B1ir,R1234rs --ae_autoquant 0 BN BR1234c
-popd
